9e820d2d328d47038e0270ee9a6e8b9abbaa69c3
ashish2508 <jha250805@gmail.com>
2024-12-28 06:14:12 +0530

    making routes for todos opearation

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -1,18 +1,22 @@
-import express from 'express'
-import db from '../db.js'
+import express from "express";
+import db from "../db.js";
 
-const router = express.Router()
+const router = express.Router();
 
 //get all todos for logged-in user
-router.get('/',(req,res)=>{})
+router.get("/", (req, res) => {
+	const getTodos = db.prepare(`SELECT * FROM todos WHERE user_id=?`);
+	const todos = getTodos.all(req.userId);
+	res.json(todos);
+});
 
 //Create a new todo
-router.post('/',(req,res)=>{})
+router.post("/", (req, res) => {});
 
 //update a todo
-router.post('/:id',(req,res)=>{})
+router.post("/:id", (req, res) => {});
 
 //delete a todo
-router.delete('/:id',(req,res)=>{})
+router.delete("/:id", (req, res) => {});
 
-export default router
+export default router;
