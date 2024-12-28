ebad4e64d8f9ee68cd1e7f402b9faef67e75979a
ashish2508 <jha250805@gmail.com>
2024-12-28 11:29:17 +0530

    added todo store routes

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -23,14 +23,26 @@ router.post("/", (req, res) => {
 
 //update a todo
 router.put("/:id", (req, res) => {
+	const { completed } = req.body;
+	const { id } = req.params;
+	// for page wise change and destructuring from query
+	//const {page}=req.query
 	const updatedTodo = db.prepare("UPDATE todos SET completed = ? WHERE id = ?");
+	updatedTodo.run(completed, id);
+
+	res.json({ message: "Todo completed" });
 });
 
 //delete a todo
 router.delete("/:id", (req, res) => {
+	const { id } = req.params;
+	const userId = req.userId;
 	const deleteTodo = db.prepare(
 		`DELETE FROM todos WHERE id = ? AND user_id = ?`
 	);
+	deleteTodo.run(id, userId);
+
+	res.send({ message: "Todo deleted" });
 });
 
 export default router;
